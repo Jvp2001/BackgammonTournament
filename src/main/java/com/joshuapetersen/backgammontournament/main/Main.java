ad27aa9787a2878594ed694cd955391113f3ceb1
jvp2001 <thisisminecraftpetersen@gmail.com>
2019-07-17 08:42:00 +0100

    State ======================================= Application Works but does not save yet.
    Changes
    =======================================
    Fixed some problems with names.
    Fixed a bug in sample.fxml file.

--- src/main/java/com/joshuapetersen/backgammontournament/main/Main.java
+++ src/main/java/com/joshuapetersen/backgammontournament/main/Main.java
@@ -6,16 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static com.joshuapetersen.backgammontournament.main.BackgammonTournamentConfiguration.*;
+
 public class Main extends Application
 {
-
-    public static final int APP_WIDTH = 800, APP_HEIGHT = 600;
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle(TITLE);
         primaryStage.setScene(new Scene(root, APP_WIDTH, APP_HEIGHT));
         primaryStage.show();
     }
