862a04de2b8b15e595028f0ed1a94ab7cc775b06
jvp2001 <thisisminecraftpetersen@gmail.com>
2019-07-18 12:48:32 +0100

    Revert "State ======================================= Application Works but does not save yet."
    This reverts commit ad27aa97

--- src/main/java/com/joshuapetersen/backgammontournament/main/BackgammonTournamentConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.joshuapetersen.backgammontournament.main;
-
-public class BackgammonTournamentConfiguration
-{
-    public static final int APP_WIDTH = 800, APP_HEIGHT = 600;
-    public static final String TITLE = "Backgammon Tournament";
-}
