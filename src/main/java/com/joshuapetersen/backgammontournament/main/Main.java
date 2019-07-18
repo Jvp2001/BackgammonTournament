2d283ad191ecd34f97d7bdff65ac0e0ceb59c4fc
jvp2001 <thisisminecraftpetersen@gmail.com>
2019-07-18 17:30:25 +0100

    Fully Working Application

--- src/main/java/com/joshuapetersen/backgammontournament/main/Main.java
+++ src/main/java/com/joshuapetersen/backgammontournament/main/Main.java
@@ -11,14 +11,16 @@ public class Main extends Application
 {
 
     public static final int APP_WIDTH = 800, APP_HEIGHT = 600;
-
+    public static Stage stage;
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+        stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/sample.fxml"));
         primaryStage.setTitle("Backgammon Tournament");
         primaryStage.setScene(new Scene(root, APP_WIDTH, APP_HEIGHT));
         primaryStage.getIcons().setAll(new Image("/images/iconbig.png"));
+        primaryStage.getScene().getStylesheets().add("/stylesheets/DarkMode.css");
         primaryStage.show();
     }
 
